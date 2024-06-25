1dff1be27fc5724b167bfba06fca172deb4f9cbc
ogatank <102461999+ogatank@users.noreply.github.com>
2024-06-25 15:45:24 +0900

    ファイルをアップロード

---
+++ +αLP/script.js
@@ -0,0 +1,9 @@
+// 例：お問い合わせフォームのバリデーション
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // フォームの入力値をチェックする処理を実装する
+    // 例：必須項目のチェックやフォーマットのチェック
+    // 入力が正当であれば、サーバーにフォームを送信するなどの処理を追加する
+});
